e2441748456e8c4ceeb4c5a6d8e22be0f77dd227
Mindaugas <darbas.mindaugas@gmail.com>
2023-02-02 18:38:13 +0200

    Creating the full project structure

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import logging
+
+logging.basicConfig(
+    filename='logs/main.log',
+    level=logging.INFO
+)
+
+
+def add(i, j):
+  return i + j
+
+
+def multiply(i, j):
+  return i * j
+
+
+res_add = add(3, 2)
+res_multiply = multiply(3, 2)
+
+logging.info('{} : {}'.format('Addition result', res_add))
+logging.info('{} : {}'.format('Multiplication result', res_multiply))
+
+
 print("Helo")
 print("Helo")
 print("Hello!")
